2269c8c893e4138690bb8df9db8109f118d59bb4
Adisakshya Chauhan <45028982+adisakshya@users.noreply.github.com>
2019-01-11 20:00:19 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,23 @@
-print("Setup Comes Here")
+"""
+Module name: setup.py
+Author: Adisakshya Chauhan
+"""
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'cryptographer',
+    'author': 'Adisakshya Chauhan',
+    'url': 'URL to get it at.',
+    'download_url': 'Where to download it.',
+    'author_email': 'adisakshya chauhan',
+    'version': '1.0',
+    'install_requires': ['nose','Crypto'],
+    'packages': ['crypto'],
+    'scripts': ['bin/crypto.py'],
+    'name': 'crypto'
+    }
+
+setup(**config)
