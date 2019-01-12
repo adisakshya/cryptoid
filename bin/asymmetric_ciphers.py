dbcc60fa398877699f254b66271544c22ec7e886
Adisakshya Chauhan <45028982+adisakshya@users.noreply.github.com>
2019-01-12 15:57:15 +0530

    Update asymmetric_ciphers.py

--- bin/asymmetric_ciphers.py
+++ bin/asymmetric_ciphers.py
@@ -17,13 +17,13 @@ class rsa(object):
         return RSA.generate(size)
 
     # function to encrypt message    
-    def rsa_encrypt(rsa_public_key, message):
+    def encrypt(rsa_public_key, message):
 
         obj = PKCS1_OAEP.new(rsa_public_key)
         return obj.encrypt(message.encode('utf-8'))
 
     # function to decrypt message
-    def rsa_decrypt(rsa_key, cipher_text):
+    def decrypt(rsa_key, cipher_text):
 
         obj2 = PKCS1_OAEP.new(rsa_key)
         return obj2.decrypt(cipher_text)
@@ -33,14 +33,31 @@ Viginere Cipher
 """
 class viginere_cipher(object):
 
-    def encrypt(plaintext, key):
+    def encrypt(clear, key):
         
-        pass
+        enc = [] 
+      
+        for i in range(len(clear)): 
+            key_c = key[i % len(key)] 
+            enc_c = chr((ord(clear[i]) + ord(key_c)) % 256) 
+                       
+            enc.append(enc_c) 
+          
+        return "".join(enc) 
+  
 
 
-    def decrypt(ciphertext, key):
+    def decrypt(enc, key):
         
-        pass
+        dec = [] 
+      
+        enc = base64.urlsafe_b64decode(enc).decode() 
+        for i in range(len(enc)): 
+            key_c = key[i % len(key)] 
+            dec_c = chr((256 + ord(enc[i]) - ord(key_c)) % 256) 
+                             
+            dec.append(dec_c) 
+        return "".join(dec)
 
 """
 Caesar Cipher
@@ -78,134 +95,19 @@ PlayFair Cipher
 """
 
 class playfair_cipher(object):   
-
-    def chunker(seq, size):
-        
-        it = iter(seq)
-        while True:
-            
-            chunk = tuple(itertools.islice(it, size))
-            if not chunk:
-            
-                return
-            
-            yield chunk
-
-    def prepare_input(dirty):
-        
-        """
-        Prepare the plaintext by up-casing it
-        and separating repeated letters with X's
-        """
     
-        dirty = ''.join([c.upper() for c in dirty if c in string.ascii_letters])
-        clean = ""
-    
-        if len(dirty) < 2:
-        
-            return dirty
+    def encrypt():
 
-        for i in range(len(dirty)-1):
-        
-            clean += dirty[i]
-        
-            if dirty[i] == dirty[i+1]:
-        
-                clean += 'X'
-    
-        clean += dirty[-1]
+        pass
 
-        if len(clean) & 1:
-        
-            clean += 'X'
+    def decrypt():
 
-        return clean
-
-    def generate_table(key):
-
-        # I and J are used interchangeably to allow
-        # us to use a 5x5 table (25 letters)
-        alphabet = "ABCDEFGHIKLMNOPQRSTUVWXYZ"
-        # we're using a list instead of a '2d' array because it makes the math 
-        # for setting up the table and doing the actual encoding/decoding simpler
-        table = []
-
-        # copy key chars into the table if they are in `alphabet` ignoring duplicates
-        for char in key.upper():
-        
-            if char not in table and char in alphabet:
-                table.append(char)
-
-        # fill the rest of the table in with the remaining alphabet chars
-        for char in alphabet:
-        
-            if char not in table:
-                table.append(char)
-
-        return table
-
-    def encrypt(plaintext, key):
-        
-        table = generate_table(key)
-        plaintext = prepare_input(plaintext)
-        ciphertext = ""
-
-        # https://en.wikipedia.org/wiki/Playfair_cipher#Description
-        for char1, char2 in chunker(plaintext, 2):
-        
-            row1, col1 = divmod(table.index(char1), 5)
-            row2, col2 = divmod(table.index(char2), 5)
-
-            if row1 == row2:
-        
-                ciphertext += table[row1*5+(col1+1)%5]
-                ciphertext += table[row2*5+(col2+1)%5]
-        
-            elif col1 == col2:
-        
-                ciphertext += table[((row1+1)%5)*5+col1]
-                ciphertext += table[((row2+1)%5)*5+col2]
-        
-            else: # rectangle
-        
-                ciphertext += table[row1*5+col2]
-                ciphertext += table[row2*5+col1]
-
-        return ciphertext
-
-
-    def decrypt(ciphertext, key):
-        
-        table = generate_table(key)
-        plaintext = ""
-
-        # https://en.wikipedia.org/wiki/Playfair_cipher#Description
-        for char1, char2 in chunker(ciphertext, 2):
-        
-            row1, col1 = divmod(table.index(char1), 5)
-            row2, col2 = divmod(table.index(char2), 5)
-
-            if row1 == row2:
-        
-                plaintext += table[row1*5+(col1-1)%5]
-                plaintext += table[row2*5+(col2-1)%5]
-        
-            elif col1 == col2:
-        
-                plaintext += table[((row1-1)%5)*5+col1]
-                plaintext += table[((row2-1)%5)*5+col2]
-        
-            else: # rectangle
-        
-                plaintext += table[row1*5+col2]
-                plaintext += table[row2*5+col1]
-
-        return plaintext
+        pass
 
 """
 Polybius Square Cipher
 """
-def polybius_square_cipher(oject):
+class polybius_square_cipher(object):
     
     def encrypt(s):
 
