8b7d6ac0bd3f1df8082621d6d7d6ab58d81aa8b1
Adisakshya Chauhan <45028982+adisakshya@users.noreply.github.com>
2019-01-19 18:46:05 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ config = {
     'download_url': 'https://github.com/adisakshya/cryptoid',
     'author_email': 'adisakshya98@gmail.com',
     'version': '1.0',
-    'install_requires': ['nose','Crypto'],
-    'packages': ['cryptoid'],
+    'install_requires': ['Crypto', 'codecs', 'string', 'setuptools', 'distutils'],
+    'packages': ['Crypto', 'codecs', 'string'],
     'scripts': ['bin/cryptoid.py'],
     'name': 'cryptoid'
     }
